1b31fe4ce512c845f0af68d22d01f3db3789c761
kuba <kuba@kuba-laptop>
2013-08-07 10:47:07 +0200

    delete docs

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","__autoload()"],["c","Attribute"],["c","AttributeRepository"],["c","Database"],["c","Edge"],["c","EdgeRepository"],["c","Hindrance"],["c","HindranceRepository"],["c","PDO"],["c","Power"],["c","PowerRepository"],["c","Race"],["c","Sheet"],["c","SheetRepository"],["c","Skill"],["c","SkillRepository"],["c","User"]];
