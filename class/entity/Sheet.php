54201579bab29b9e591aa664a8e5a7c4874c4261
Gogi <Gogi@Gogi>
2013-08-13 22:34:31 +0200

    Pobieranie wartości z danej karty do formularza.

--- class/entity/Sheet.php
+++ class/entity/Sheet.php
@@ -21,6 +21,56 @@ class Sheet
 	private $skills;
 	private $attributes;
 	private $powers;
+	
+		public function getAttribute($id)
+	{
+		for ($i = 0; $i < count($this->attributes); $i++) {
+			if ($this->attributes[$i]->getId() == $id) {
+				return $this->attributes[$i];
+			}
+		}
+		return false;
+	}
+
+	public function getSkill($id)
+	{
+		for ($i = 0; $i < count($this->skills); $i++) {
+			if ($this->skills[$i]->getId() == $id) {
+				return $this->skills[$i];
+			}
+		}
+		return false;
+	}
+
+	public function getEdge($id)
+	{
+		for ($i = 0; $i < count($this->edges); $i++) {
+			if ($this->edges[$i]->getId() == $id) {
+				return $this->edges[$i];
+			}
+		}
+		return false;
+	}
+
+	public function getHindrance($id)
+	{
+		for ($i = 0; $i < count($this->hindrances); $i++) {
+			if ($this->hindrances[$i]->getId() == $id) {
+				return $this->hindrances[$i];
+			}
+		}
+		return false;
+	}
+
+	public function getPower($id)
+	{
+		for ($i = 0; $i < count($this->powers); $i++) {
+			if ($this->powers[$i]->getId() == $id) {
+				return $this->powers[$i];
+			}
+		}
+		return false;
+	}
 
 	public function getId()
 	{
