40f4b7564a305c99a9815ef7e3678a0d5e701882
e-price <e-price@10.0.0.65>
2013-08-17 13:22:50 +0200

    adamos

--- controller/SheetController.php
+++ controller/SheetController.php
@@ -5,67 +5,74 @@
  *
  * @author kuba
  */
-class SheetController extends Controller {
+class SheetController extends Controller
+{
 
-    public function showAction($id = null) {
+	public function showAction($id = null)
+	{
 
-        $baseAttributeRepository = new BaseAttributeRepository();
-        $attributes = $baseAttributeRepository->getAll();
+		$baseAttributeRepository = new BaseAttributeRepository();
+		$attributes = $baseAttributeRepository->getAll();
 
-        $baseSkillRepository = new BaseSkillRepository();
-        $skills = $baseSkillRepository->getAll('name');
+		$baseSkillRepository = new BaseSkillRepository();
+		$skills = $baseSkillRepository->getAll('name');
 
-        $edgeRepository = new EdgeRepository();
-        $edges = $edgeRepository->getAll();
+		$edgeRepository = new EdgeRepository();
+		$edges = $edgeRepository->getAll();
 
-        $hindranceRepository = new HindranceRepository();
-        $hindrances = $hindranceRepository->getAll();
+		$hindranceRepository = new HindranceRepository();
+		$hindrances = $hindranceRepository->getAll();
 
-        $powerRepository = new PowerRepository();
-        $powers = $powerRepository->getAll();
+		$powerRepository = new PowerRepository();
+		$powers = $powerRepository->getAll();
 
-        $raceRepository = new RaceRepository();
-        $races = $raceRepository->getAll();
+		$raceRepository = new RaceRepository();
+		$races = $raceRepository->getAll();
 
-        $userRepository = new UserRepository();
-        $users = $userRepository->getAll();
+		$userRepository = new UserRepository();
+		$users = $userRepository->getAll();
 
-        $sheetRepository = new SheetRepository();
-        if ($id) {
-            $sheet = $sheetRepository->getById($id);
-        } else {
-            $sheet = null;
+		$sheetRepository = new SheetRepository();
+		if ($id) {
+			$sheet = $sheetRepository->getById($id);
+		} else {
+			$sheet = null;
+		}
+
+		echo $this->getView()->render('form.html.twig', array('attributes' => $attributes, 'skills' => $skills, 'edges' => $edges, 'hindrances' => $hindrances, 'powers' => $powers, 'races' => $races, 'users' => $users, 'sheet' => $sheet));
+	}
+
+	public function formAction()
+	{
+		$config = SimpleConfig::getInstance();
+		$request = $config->request;
+
+		echo '<pre>';
+		print_r($request->getPost());
+		echo '</pre>';
+		exit();
+	}
+
+	public function listAction()
+	{
+
+		$sheetRepository = new SheetRepository();
+		$sheets = $sheetRepository->getAllSimple();
+
+		echo $this->getView()->render('list.html.twig', array('sheets' => $sheets));
+	}
+
+	public function removeAction($id)
+	{
+		$sheetRepository = new SheetRepository();
+		$sheetRepository->remove($id);
+	}
+        
+        public function simpleAction($id) {
+            $rep = new SheetRepository();
+            $sheet = $rep->getById($id);
+            echo $this->getView()->render('simple.html.twig', array("sheet" => $sheet));
         }
-
-        echo $this->getView()->render('form.html.twig', array('attributes' => $attributes, 'skills' => $skills, 'edges' => $edges, 'hindrances' => $hindrances, 'powers' => $powers, 'races' => $races, 'users' => $users, 'sheet' => $sheet));
-    }
-
-    public function formAction() {
-        $config = SimpleConfig::getInstance();
-        $request = $config->request;
-
-        $formRead = new FormRead();
-        $sheet = $formRead->getForm();
-        
-       
-        
-        $sr = new SheetRepository();
-        $sr->persist($sheet, false);
-        exit();
-    }
-
-    public function listAction() {
-
-        $sheetRepository = new SheetRepository();
-        $sheets = $sheetRepository->getAllSimple();
-
-        echo $this->getView()->render('list.html.twig', array('sheets' => $sheets));
-    }
-
-    public function removeAction($id) {
-        $sheetRepository = new SheetRepository();
-        $sheetRepository->remove($id);
-    }
 
 }
 
